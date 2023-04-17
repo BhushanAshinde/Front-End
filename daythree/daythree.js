5c92abd2e90b52df89840ed34a03cd76f012bca3
BhushanAshinde <92881526+BhushanAshinde@users.noreply.github.com>
2023-04-17 15:51:11 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,32 @@
+/*alert("Congratulations")
+alert("hello")
+alert(4+2)
+let num=15;
+    alert(num)
+//const    x=12;
+//alert(x);
+var x=15;
+alert(x);
+
+var   y=50;
+alert(y);
+
+let a=5;
+    sum=a+y;
+    alert(sum)*/
+
+    //relational operator
+    var res=12==0;
+
+    //logical operator
+    y=5;
+    x=9;
+    alert(x==y&& x>y)
+
+    if(x>y)
+        alert("harshada is cute girl but barakale is my best frnd")
+    else
+        alert("but she gets angry on me")
+
+    let age=prompt("what,s your age",100)
+    alert("your age is :"+age)
